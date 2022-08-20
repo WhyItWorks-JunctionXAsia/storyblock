e58084a69e01e9dff063cb25c3d02909ecc65d3b
Donghee John <aminor32dh@gmail.com>
2022-08-20 12:18:19 +0900

    Add global browser router

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "index.css";
 import App from "App";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
