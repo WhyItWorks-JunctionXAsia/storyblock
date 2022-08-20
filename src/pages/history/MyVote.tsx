ab51835a7d9d9c65960a97037f194da7fa307f21
Donghee John <aminor32dh@gmail.com>
2022-08-20 17:28:35 +0900

    Add more button

--- src/pages/history/MyVote.tsx
+++ src/pages/history/MyVote.tsx
@@ -3,6 +3,9 @@ import Area from "components/Area";
 import ContentArea from "components/ContentArea";
 import Text from "components/Text";
 import VoteCard from "./VoteCard";
+import FlexContainer from "components/FlexContainer";
+import Button from "components/Button";
+import { colorset } from "utils/styles";
 
 const MyVote: React.FC = () => {
   return (
@@ -15,6 +18,7 @@ const MyVote: React.FC = () => {
             display: "grid",
             gridTemplateColumns: "1fr 1fr",
             gap: "30px 30px",
+            marginBottom: "50px",
           }}
         >
           <VoteCard />
@@ -22,6 +26,19 @@ const MyVote: React.FC = () => {
           <VoteCard />
           <VoteCard />
         </div>
+
+        <FlexContainer justifyContent="center">
+          <Button
+            style={{
+              width: "120px",
+              height: "50px",
+              borderRadius: "25px",
+              background: colorset.white,
+            }}
+          >
+            <Text>MORE</Text>
+          </Button>
+        </FlexContainer>
       </ContentArea>
     </Area>
   );
