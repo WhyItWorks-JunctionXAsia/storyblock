218b7359b4c64999a227cd053d3e9cded470b89b
Donghee John <aminor32dh@gmail.com>
2022-08-21 11:27:04 +0900

    Fix unterminated JSX element error

--- src/pages/account/AccountPage.tsx
+++ src/pages/account/AccountPage.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import FlexContainer from "components/FlexContainer";
-import accountImage from "assets/images/account.png"
-import Area from "../../components/Area";
-import ContentArea from "../../components/ContentArea";
+import accountImage from "assets/images/account.png";
+import Area from "components/Area";
+import ContentArea from "components/ContentArea";
 
 const AccountPage: React.FC = () => {
-    return (
-        <Area>
-            <ContentArea>
-                <img src={accountImage} width={"100%"}/>
-
-
-
-    );
+  return (
+    <Area>
+      <ContentArea>
+        <img src={accountImage} width={"100%"} />
+      </ContentArea>
+    </Area>
+  );
 };
-export default AccountPage
+export default AccountPage;
