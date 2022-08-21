801daec5d55306e23536de43cd4de8fed18bd019
chungjung <chungjung@DESKTOP-AH2B8FO>
2022-08-21 11:58:53 +0900

    account

--- src/pages/home/HomeBanner.tsx
+++ src/pages/home/HomeBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import backgroundImage from 'assets/images/listpage-background.png'
 import FlexContainer from "components/FlexContainer";
 import homeBanner from "assets/images/home-banner.png"
-import VoteStory from "../../components/VoteStory";
+
 
 const HomeBanner: React.FC = () => {
     return (
