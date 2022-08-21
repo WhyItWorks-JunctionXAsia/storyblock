824b1a1cfe33c55b1bd6d9e65044197febad6f78
chungjung <chungjung@DESKTOP-AH2B8FO>
2022-08-21 11:24:28 +0900

    temp

--- src/components/VoteStory.tsx
+++ src/components/VoteStory.tsx
@@ -3,15 +3,42 @@ import React from "react";
 import FlexContainer from "components/FlexContainer";
 import homeBanner from "assets/images/home-banner.png"
 import Button from "./Button";
+import styled from "styled-components";
+import close from "assets/icons/close.svg"
+import search from "../assets/icons/search-black.svg";
+
+
+const VoteTextArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  height: 800px;
+  
+  ::-webkit-scrollbar{
+    display: none;
+    width: 0;
+  }
+`;
+
 
 const VoteStory: React.FC = () => {
     return (
-        <FlexContainer flexDirection={"column"} style={{width:"1300px"}}>
-            <FlexContainer></FlexContainer>
-            <div style={{ scrollbarWidth:"none", overflowY:"scroll", width:"100%" , height:"930px"}}>
+        <FlexContainer flexDirection={"column"} style={{width:"700px"}}>
+            <FlexContainer  justifyContent={"space-between"} style={{height:"150px" ,width:"100%"}}>
+                <FlexContainer>
+
+                </FlexContainer>
+                <Button>
+                    <img src={close} width="36px" />
+                </Button>
+            </FlexContainer>
+            <VoteTextArea>
                 dewdewdewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            </div>
-            <FlexContainer>
+            </VoteTextArea>
+
+            <FlexContainer justifyContent={"center"}>
                 <Button>
                     vote
                 </Button>
